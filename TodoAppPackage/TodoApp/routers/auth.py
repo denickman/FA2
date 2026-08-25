180755571d791c23c81ff2c1e24bf1e3b52fa643
Dennis Yaremenko <yaremdennis@gmail.com>
2026-08-25 14:33:48 +0300

    df

--- TodoAppPackage/TodoApp/routers/auth.py
+++ TodoAppPackage/TodoApp/routers/auth.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta, datetime, timezone
 
 from fastapi import APIRouter, HTTPException, Depends
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from pydantic import BaseModel
 from typing import Annotated
 from starlette import status
@@ -11,7 +11,7 @@ from models import Users
 from routers.dependencies import db_dependency
 from passlib.context import CryptContext
 
-from jose import jwt
+from jose import jwt, JWTError
 
 router = APIRouter(
     prefix='/auth',
@@ -23,6 +23,8 @@ SECRET_KEY = 'a506f3d30e634ea8f80fe5ffbbb1627b3813b3f1e7d6a54686efd78830a99299'
 ALGORITHM = 'HS256'
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
+# ✅ исправлено
 
 
 class CreateUserRequest(BaseModel):
@@ -41,17 +43,30 @@ class Token(BaseModel):
 
 
 def create_access_token(username: str, user_id: int, expires_delta: timedelta = None):
-
     encode = {
         'sub': username,
         'id': user_id,
     }
 
     expires = datetime.now(timezone.utc) + expires_delta
-
     encode.update({'exp': expires})
-
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+
+        return {'username': username, 'user_id': user_id}
+
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
 
 
 
@@ -148,8 +163,3 @@ async def login_for_access_token(
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
 
     return {"access_token": token, "token_type": "bearer"}
-
-
-@router.get('/me', status_code=status.HTTP_200_OK)
-async def get_current_user():
-    return {'user': 'authenticated'}
