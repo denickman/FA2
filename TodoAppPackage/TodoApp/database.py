9f5fadd2caaba617755cc15c92216d507ad7ef9a
Dennis Yaremenko <yaremdennis@gmail.com>
2026-08-27 11:38:09 +0300

    Add Postgres SQL

--- TodoAppPackage/TodoApp/database.py
+++ TodoAppPackage/TodoApp/database.py
@@ -3,11 +3,27 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
+# for local sqlite DB
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# for Postgres SQL
+# name of DB should be the same as you set it in pgAdmin4 programm
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:qwerty@localhost/ToDoAppDatabase'
+# postgresql://postgres:qwerty@localhost/ToDoAppDatabase
+#     ↓           ↓       ↓        ↓            ↓
+#   драйвер    юзер    пароль    хост       имя базы данных
+
+
+
+# for local sqlite DB
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+
+# for Postgres SQL
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
