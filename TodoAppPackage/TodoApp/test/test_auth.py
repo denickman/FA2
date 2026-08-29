5ad0639b4ed9099e058cf30cdcb0ecafe64a2f4a
Dennis Yaremenko <yaremdennis@gmail.com>
2026-08-29 19:28:56 +0300

    ad

--- TodoAppPackage/TodoApp/test/test_auth.py
+++ TodoAppPackage/TodoApp/test/test_auth.py
@@ -1,7 +1,11 @@
 from passlib.hosts import netbsd_context
 
 from .utils import *
-from ..routers.auth import get_db, authenticate_user
+from ..routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM, get_current_user
+from jose import jwt
+from datetime import datetime, timedelta
+import pytest
+from fastapi import HTTPException
 
 
 app.dependency_overrides[get_db] = override_get_db
@@ -19,3 +23,62 @@ def test_authenticate_user(test_user):
 
     wrong_password_user = authenticate_user(db, 'Wrong password', '')
     assert wrong_password_user is None
+
+
+
+def test_create_access_token():
+    username = 'testuser'
+    user_id = 1
+    role = 'user'
+    expires_delta = timedelta(days=1)
+
+    token = create_access_token(user_id, username, role, expires_delta)
+
+    decoded_token = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM], options={'verify_signature': False})
+
+    assert decoded_token['sub'] == username
+    assert decoded_token['id'] == user_id
+    assert decoded_token['role'] == role
+
+
+
+@pytest.mark.asyncio
+async def test_get_current_user_valid_token():
+    encode = {'sub': 'testuser', 'id': 1, 'role': 'admin'}
+    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    user = await get_current_user(token=token)
+    assert user == {'user_id': 1, 'username': 'testuser', 'user_role': 'admin'}
+
+
+
+@pytest.mark.asyncio
+async def test_get_current_user_invalid_token():
+    encode = {'role': 'user'}
+    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    with pytest.raises(HTTPException) as excinfo:
+        await get_current_user(token=token)
+
+    assert excinfo.value.status_code == 401
+    assert excinfo.value.detail == 'Incorrect username or password'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
